dc2ef70c4320f68694502b4fb68679b89a4702ca
Misa Liu <misaliu@misaliu.top>
2026-04-28 16:49:37 +0800

    feat(wiki): Add item store

---
+++ packages/wiki/src/store/item.ts
@@ -0,0 +1,32 @@
+import 'preact/compat';
+import { create } from 'zustand';
+import type { Item } from '@manosaba/types';
+
+interface ItemStore {
+  items: Item[],
+  loading: boolean,
+  error: string | null,
+  fetchItems: () => Promise<void>,
+}
+
+export const useItemStore = create<ItemStore>((set, get) => ({
+  items: [],
+  loading: false,
+  error: null,
+  fetchItems: async () => {
+    if (get().loading) return;
+    if (get().error) return;
+    if (get().items.length > 0) return;
+
+    set({ loading: true, error: null });
+
+    try {
+      const response = await fetch('/items.json');
+      const items = await response.json();
+      console.log(items);
+      set({ items, loading: false });
+    } catch (error) {
+      set({ loading: false, error: (error as Error).message ?? JSON.stringify(error) });
+    }
+  }
+}));
